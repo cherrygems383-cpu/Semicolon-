982891c2219a46aa2e3697354ef49b6907599eb6
cherrygems383-cpu <cherrygems383@gmail.com>
2026-09-23 08:49:18 +0100

    done again

--- codingquest/Receipt.java
+++ codingquest/Receipt.java
@@ -1,17 +1,19 @@
 public class Receipt {
 public static void main (String []   args) {
 
-  String item = "Jellof rice";
+   String item = "Jellof rice";
   int plates = 3;
-  double price = 1250.5;
+   double price = 1250.5;
   
   System.out.printf("Item:  %s%n",  item) ;
   System.out.printf("Plates: %d%n", plates) ;
   System.out.printf("Price: %.2f%n", price) ;
   System.out.printf("Total: %.2f%n", plates * price) ;
   System.out.println() ;
-  System.out.printf( "%-12%5s%10s%n",  "Item", "Qty", "Cost" ) ;
-  System.out.printf( "%-12%5s%10s%n",  item, plates, plates * price) ;
+  System.out.printf("%-12s%5s%10s%n", "Item", "Qty", "Cost") ;
+  System.out.printf("%-12s%5d%10.2f%n", item, plates, plates * price) ;
+
+
       }
           }
     
